aa30918e64bf2cb38094a1e12559e802b5d1efd2
diegoc1005 <diego05102005@gmail.com>
2025-03-06 14:09:05 -0600

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,1 +1,18 @@
+// Diego Córdova
+// ejercicio 1 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+    { 
+        int a=3;
+        int b=2;
+        
+        if (a>b)
+        {   
+            printf("a es mayor");
+        }
+        return 0;
+    }
+    
